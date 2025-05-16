6411b7559c7fa7b8760032cf409b1f330fd9cc49
Corbin Jensen <corbin.jensen@me.com>
2025-05-16 15:29:46 -0400

    initial commit

---
+++ types/Ministry.ts
@@ -0,0 +1,20 @@
+export interface Ministry {
+  id: number;
+  slug: string;
+  name: string;
+  summary: string;
+  category: string;
+  logo: string;
+  cover_photo: string;
+  website: string;
+  facebook_url: string;
+  instagram_url: string;
+  youtube_url: string;
+  x_url: string;
+  tiktok_url: string;
+  blog_url: string;
+  created_at: string;
+  updated_at: string;
+  tagline: string;
+  summary_html: string;
+}
